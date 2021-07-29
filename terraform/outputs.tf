61e6c36b867485a37817d1fdc6fbd0b7040aa191
Aleksandr <avo1yanskiy@mail.ru>
2021-07-29 23:09:17 +0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,8 +12,3 @@ output "caller_user" {
   value = data.aws_caller_identity.current.user_id
 }
 data "aws_region" "current" {}
-
-output "instance_ip_addr" {
-  value = aws_instance.ubuntu.private_ip
-}
-    
