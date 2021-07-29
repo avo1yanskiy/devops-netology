200bacee715fc7f44fd8f902b90ccffd1e775a3b
Aleksandr <avo1yanskiy@mail.ru>
2021-07-29 23:50:28 +0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -23,3 +23,4 @@ output "endpoint" {
 output "instance_public_ip" {
   description = "List of public IP addresses assigned to the instances, if applicable"
   value       = aws_instance.ubuntu.public_ip
+}
