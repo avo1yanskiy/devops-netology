40d5c99f2857f48b0480bbbee6df7cfec0676e7e
Aleksandr <avo1yanskiy@mail.ru>
2021-07-30 00:02:21 +0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -20,12 +20,12 @@ output "endpoint" {
         value = data.aws_region.current.endpoint
 }
 
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.web.id
-}
+#output "instance_id" {
+#  description = "ID of the EC2 instance"
+#  value       = aws_instance.web.id
+#}
 
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.web.public_ip
-}
+#output "instance_public_ip" {
+#  description = "Public IP address of the EC2 instance"
+#  value       = aws_instance.web.public_ip
+#}
