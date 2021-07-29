34eead0906dd19404558adaa7d0426bdfc67d149
Aleksandr <avo1yanskiy@mail.ru>
2021-07-29 21:59:52 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,7 +28,26 @@ root_block_device {
   volume_type        = "gp2"
 }
 
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
+
+output "caller_user" {
+  value = data.aws_caller_identity.current.user_id
+}
+
+data "aws_region" "current" {
+    name = "us-west-2"
+}
+
   tags = {
+
     Name = "My First Install"
    }
  }
