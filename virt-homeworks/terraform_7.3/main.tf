181c71d20fffc7e8bd752f5f8761bb7c08bcf2b3
Aleksandr.Volyanskiy <avo1yanskiy@mail.ru>
2021-08-09 13:24:03 +0300

    add terraform

---
+++ virt-homeworks/terraform_7.3/main.tf
@@ -0,0 +1,39 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "ServerWeb" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  tags = {
+    Name = "My First Install AWS"
+  }
+  root_block_device {
+          delete_on_termination = false
+          volume_size           = 8
+          volume_type           = "gp2"
+        }
+}
+ 
---
+++ virt-homeworks/terraform_7.3/outputs.tf
@@ -0,0 +1,31 @@
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
+
+output "caller_user" {
+  value = data.aws_caller_identity.current.user_id
+}
+data "aws_region" "current" {}
+
+output "name" {
+        value = data.aws_region.current.name
+}
+output "endpoint" {
+        value = data.aws_region.current.endpoint
+}
+
+output "instance_id" {
+    description = "ID of the EC2 instance"
+    value       = aws_instance.ServerWeb.id
+}
+
+output "instance_public_ip" {
+    description = "Public IP address of the EC2 instance"
+    value       = aws_instance.ServerWeb.public_ip
+}
