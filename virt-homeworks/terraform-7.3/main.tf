d5031411f54eaa69b628133047bb1a962240e978
Aleksandr Volyanskiy <54029550+avo1yanskiy@users.noreply.github.com>
2021-08-09 21:54:08 +0300

    Update main.tf

--- virt-homeworks/terraform-7.3/main.tf
+++ virt-homeworks/terraform-7.3/main.tf
@@ -19,6 +19,15 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+resource "aws_s3_bucket" "bucket" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "bucket-test-7-3"
+    Environment = "test"
+  }
+}
 resource "aws_instance" "ServerWeb" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
